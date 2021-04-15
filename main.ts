1856be8bc8560a21278e79dd511a6aecc8cd53e5
koideMiru <82562230+koideMiru@users.noreply.github.com>
2021-04-15 14:03:51 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Heart)
+    music.playMelody("E B C5 A B G A F ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
